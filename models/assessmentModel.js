da99e5d0d5a98deafed085ae293c4286fca47b48
stphnrosa <stphn.rosa@gmail.com>
2025-05-12 09:41:26 -0400

    created base model

--- models/assessmentModel.js
+++ models/assessmentModel.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'; // need monogoose to createschema
 
 const assessmentSchema = new mongoose.Schema ({
     title: {type:String, require: true },
-    body: {type: String},
+    body: {type: String}
 }, {
-    timestamps:true
+    timestamps: true // this automatically creates a created app field and an updated app field
 });
 
-const Assessment= mongoose.model('Assessment', assessmentSchema); // first argument is name of model. This is what you see created in MongoDB, Second argument is the schema theat the documents need to follow when they get created
+//Below is the creation of the model
+const Assessment= mongoose.model('Assessment', assessmentSchema); // first argument is name of model(This is what you see created in MongoDB). I'll have an assessment collection once i use the assessment model. Second argument is the schema that the documents need to follow when they get created.  Schema dictates what field the documents need to contain, like title since it is required above
 Assessment.create()
 
 
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import { config } from 'dotenv'; config();
 import express from 'express'; // added to package.json "type":"module", to allow import express instead of const express.Can't use import/export unless you do this.
-import cors from 'cors';
+import cors from 'cors'; // cors is used to have communication between Backend and Frontend
 import notesRouter from './routes/notes-router.js';
 import './database/database.js';
 
-
+//const declared the variable "app". we placed () to invoke express after importing it on line 2
 const app = express();
 const PORT= process.env.PORT || 3000; // user could still run the server with this port, 
 
@@ -15,5 +15,5 @@ app.use(cors());
 app.use('/notes', notesRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on PORT ${PORT}`);
+    console.log(`Server is listening on PORT ${PORT}`) //Back ticks are used for embedding a variable into a string(string interpolation). In this case ${PORT}, is embedded.
 })
