41f2295915d95b175b84e05eabae0268904f2872
stphnrosa <stphn.rosa@gmail.com>
2025-05-12 09:21:26 -0400

    corrected DB file and server

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 import {config} from 'dotenv'; config(); //invoking this immediately to use the environment variables
 import mongoose from 'mongoose';
 
+// using mongoose is what allows us to connect to our DB in mongoDB
+mongoose.connect(process.env.MONGO_URI) // Process.env.MONGO_URI is being passed as the argument in this. This variable holds the mongoose connection string
 
-mongoose.connect(process.env.MONGO_URI) 
-
-
+//Once this connection is open, we will execute this callback function. thi CB function is going to say "Connected to Database"
 Mongoose.connection.once('open', () => {
     console.log(`conncted to Database: ${mongoose.connection.name}`);
 });
