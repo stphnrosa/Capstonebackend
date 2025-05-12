56c7aa5caa4c16b9d146427b76d9b72a44ad9709
stphnrosa <stphn.rosa@gmail.com>
2025-05-12 10:38:11 -0400

    Tested assessment-controllers file with new corrections

--- controllers/assessment-controllers.js
+++ controllers/assessment-controllers.js
@@ -1,26 +1,27 @@
 import Assessment from '../models/assessmentModel.js';
 
-// async function seedAssessment(req, res) {
-//     try{
-//         await Assessment.deleteMany({});
-//         await Note.create({});
-//         {
-//             title:'',
-//             body: 'Body 1'
-//         },
-//         {
-//             title:'',
-//             body: 'Body 1'
-//         },
-//         {
-//             title:'',
-//             body: 'Body 1'
-//         },
-//     };
-//     res.status(201).json ({ success:'Database seeded.'});
-// }Catch (error) {
-//     res.stauts(400).json({error: error.message})
-// }
+async function seedAssessment(req, res) {
+    try{
+        await Assessment.deleteMany({});
+        await Assessment.create(
+        {
+            title: 'Title 1',
+            body: 'Body 1'
+        },
+        {
+            title: 'Title 2',
+            body: 'Body 2'
+        },
+        {
+            title: 'Title 3',
+            body: 'Body 3'
+        }
+      );   
+      res.status(201);
+   }  catch(error) {
+    res.status(400).json({ error: error.message });
+  }
+}
 // async function getnotes(req,res) {
 //     try{
 //         const notes= await Note.find({});
